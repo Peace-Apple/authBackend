c3d1dbf02d749661bbe3241c0fd3b2ac0ac22b01
peaceapple <astonreba@gmail.com>
2025-11-07 22:17:33 +0300

    Add login

--- user.js
+++ user.js
@@ -4,6 +4,9 @@ const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database(':memory:');
 
+const JWT_SECRET = process.env.JWT_SECRET || 'auth-secret-key';
+const nJwt = require('njwt');
+
 db.serialize(() => {
   db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, fullName TEXT, email TEXT, password TEXT)', (err) => {
         if (err) {
@@ -27,6 +30,25 @@ router.post('/register', function(req, res) {
         if (err) return res.status(500).send("An error occurred during registration");
 
         res.status(200).send({ status: 'ok' });
+    });
+});
+
+router.post('/login', function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.get('SELECT id, fullName, email, password FROM users WHERE email = ?', [email], (err, user) => {
+        if (err) return res.status(500).send({status: 'Server error', err:err});
+        if (!user) return res.status(404).send('User not found');
+
+        if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(401).send({ auth: false, token: null });
+        }
+
+        var jwt = nJwt.create({ id: user.id }, JWT_SECRET);
+        jwt.setExpiration(new Date().getTime() + (24*60*60*1000));
+
+        res.status(200).send({ auth: true, token: jwt.compact() });
     });
 });
 
